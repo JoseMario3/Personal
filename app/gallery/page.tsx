643df71b7827f25bea0ca4e9233c396a683a5141
Jose Folgar <f.jose@wustl.edu>
2025-10-05 01:32:56 -0400

    Gallery in progress

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -28,19 +28,32 @@ export default async function Gallery() {
 
   return (
     <div className={styles.body}>
-      {signedUrls.map((img) =>
-        img ? (
-          <Image
-            key={img.filename}
-            src={img.url}
-            alt={img.filename}
-            width={300}
-            height={300}
-            style={{ objectFit: "cover" }}
-            className={styles.image}
-          />
-        ) : null
-      )}
+      <div className={styles.imageWrapper}>
+        <Image
+          src={"/JennyLake.jpg"}
+          alt={"Jenny Lake"}
+          width={1000}
+          height={500}
+          className={styles.hero}
+        ></Image>
+        <span className={styles.overlayText}>Gallery</span>
+      </div>
+      {/* <div className={styles.row}></div> */}
+      <div className={styles.images}>
+        {signedUrls.map((img) =>
+          img ? (
+            <Image
+              key={img.filename}
+              src={img.url}
+              alt={img.filename}
+              width={234}
+              height={234}
+              style={{ objectFit: "cover" }}
+              className={styles.image}
+            />
+          ) : null
+        )}
+      </div>
     </div>
   );
 }
